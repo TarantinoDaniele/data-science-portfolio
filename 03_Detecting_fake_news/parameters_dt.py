5b421eef86615928c1bf910ec4ecb6a49e1a26d5
Daniele <daniele.tarantino@outlook.com>
2022-05-04 14:26:41 +0200

    fake_news

---
+++ 03_Detecting_fake_news/parameters_dt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Verwandelt Notebook in .py
+#!jupyter nbconvert --to script parameters_dt.ipynb
+
+
+# In[3]:
+
+
+# Parameters for general settings
+
+from IPython.core.display import display, HTML
+display(HTML("<style>.container { width: 80% ! important; }/<style>"))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
